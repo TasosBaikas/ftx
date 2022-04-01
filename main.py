712fab569426237b6ef2071a56167e0e7a1f33dc
tasosbaikas <tasosbaikas@gmail.com>
2022-04-01 20:11:42 +0300

    t:wq `

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import os
 from pprint import pprint
 from Ftx.FtxClient import FtxClient
 from dotenv import load_dotenv
+import datetime
 
 
 def main():
     load_dotenv()
 
-    ftx = FtxClient(str(os.getenv("API_KEY")), str(os.getenv("API_SECRET")))
+    ftx = FtxClient(str(os.getenv("API_KEY")), str(os.getenv("API_SECRET")),'pag-Perp 1')
 
-
+    data = ftx.find_how_much_all_subaccounts_won(datetime.datetime(2022,3,26).timestamp(),datetime.datetime(2022,3,28).timestamp())
+    sum = 0
+    pprint(data)
+    for i in data.values():
+        sum += i
+    print(sum)
 
 if __name__ == '__main__':
     main()
